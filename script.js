cd8505724f5a23636013e22dbddc4d5c38f6a5dc
Dzmitry Miatelitsa <Dzmitry>
2022-05-09 12:30:33 +0300

    feat: add letters

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+const container = document.createElement("div");
+container.className = "container";
+const title = document.createElement("title");
+title.className = "title";
+const textarea = document.createElement("textarea");
+textarea.className = "textarea";
+textarea.setAttribute("autofocus", "true");
+const keyboard = document.createElement("div");
